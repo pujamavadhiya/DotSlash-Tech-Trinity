4783730878fea67401b33a651af7a39f32676f33
Dev <devsapariya94@gmail.com>
2024-01-27 12:37:21 +0530

    Backend initialize

---
+++ Backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/test', methods=['POST'])
+def test():
+    link = request.form['link']
+    return jsonify({'msg': "success"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
